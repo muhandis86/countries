467900a4c5f92422ce3f93fc6150ff008ffb22a1
Zaur Eskendarov <eskendarov@gmail.com>
2024-05-03 04:46:51 +0300

    test task update

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client';
 import App from './components/app/App';
 
 import 'bootstrap/dist/css/bootstrap.min.css';
+import './index.scss';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
